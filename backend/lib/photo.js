21479b29dd11c8010787701a6cf8c18b695f70c7
Philippe Breuils <philippe.breuils@wifirst.fr>
2013-12-01 20:15:26 +0100

    Added photo ORM

---
+++ backend/lib/photo.js
@@ -0,0 +1,79 @@
+module.exports = (function() {
+  var fs = require('fs');
+  var path = require('path');
+  var im = require('imagemagick');
+  var Sync = require('sync');
+
+  var _workingDir;
+
+  _isValidFile = function(f) {
+    var re = /.*(jpeg|jpg|png)$/;
+    return re.test(path.basename(f));
+  }
+
+  _getInfo = function(name, fn) {
+    console.log(6, name);
+    var filePath = name;
+    if (path.basename(name) == name) filePath = _workingDir + '/' + name;
+    console.log(5, filePath);
+    return im.readMetadata(filePath, fn);
+  }
+
+  _listAll = function() {
+    var files = fs.readdirSync(_workingDir);
+    files = files.filter(function(f) { return _isValidFile(f); });
+    files = files.map(function(f) { return {name: f,
+                                            stat: fs.statSync(_workingDir + '/' + f)}; });
+    files = files.sort(function(b, a) { return a.stat.ctime.getTime() - b.stat.ctime.getTime(); });
+    return files;
+  }
+
+  return {
+    'setWorkingFolder': function(folder) {
+      return _workingDir = folder;
+    },
+
+    'getWorkingFolder': function() {
+      return _workingDir;
+    },
+
+    'isValid': function(f) {
+      return _isValidFile(f);
+    },
+
+    'info': function(name, fn) {
+      return _getInfo(name, fn);
+    },
+
+    // Valid options {limit: 10}
+    'all': function(options, fn) {
+      if ('function' == typeof(options)) {
+        fn = options;
+        options = {limit: 10};
+      }
+      var photos = _listAll().splice(0,options.limit);
+      var results = [];
+      var format;
+      Sync(function() {
+        for (var i = 0 ; i < photos.length ; i++) {
+          format = im.identify.sync(null, ['-format', '%wx%h', _workingDir + '/' + photos[i].name]);
+          photos[i].format = format;
+          results.push(photos[i]);
+        }
+        return results;
+      }, fn);
+      return results;
+    },
+
+    'destroy': function(name) {
+      var filePath = name;
+      if (path.basename(name) == name) filePath = _workingDir + '/' + name;
+      try {
+        fs.unlinkSync(filePath);
+        return true;
+      } catch (e) {
+        return false;
+      }
+    }
+  }
+})();
--- backend/photofinish.js
+++ backend/photofinish.js
@@ -4,10 +4,17 @@ var restify = require('restify')
   , path = require('path')
   , http = require('http');
 
+var logger = require('./lib/logger');
+
 // App constants
 var API_PORT = 8000;
 var SOCKETIO_PORT = 8001;
-var WATCH_FOLDER = './data';
+var WATCH_FOLDER = '../shared';
+
+var Photo = require('./lib/photo');
+Photo.setWorkingFolder(WATCH_FOLDER);
+
+logger.info('Photofinish starting...');
 
 // API Server
 var apiServer = restify.createServer({
@@ -15,24 +22,28 @@ var apiServer = restify.createServer({
 });
 
 apiServer.get('/photos', function getPhotos(req, res, next) {
-  res.send(201, Math.random().toString(36).substr(3, 8));
-  return next();
+  Photo.all(function(error, results) {
+    res.send(200, results);
+    return next();
+  });
 });
 
-console.log('Photofinish starting...');
-console.log('API Server listening on port', API_PORT);
+logger.info('API Server listening on port', API_PORT);
 apiServer.listen(API_PORT);
 
 // Websocket Server
-console.log('Websocket Server listening on port', SOCKETIO_PORT);
-var io = socketio.listen(SOCKETIO_PORT);
+logger.info('Websocket Server listening on port', SOCKETIO_PORT);
+
+var io = socketio.listen(SOCKETIO_PORT, {
+  'log level': 1,
+  'logger': logger
+});
 
 // Directory Watcher
 var watcher = chokidar.watch(WATCH_FOLDER, {persistent: true});
 
 watcher
   .on('add', function(filePath) {
-    var testRe = /.*(jpeg|jpg|png)$/;
-    if (!testRe.test(filePath)) return; // not a valid file format? skip this one
+    if (!Photo.isValid(filePath)) return; // not a valid file format? skip this one
     io.sockets.emit('call.added', {name: path.basename(filePath)});
   });
